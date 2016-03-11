57f572bbe7d5dccdad2d2e12216b716896a85395
ilovemacosx <ilovemacosx@sina.com>
2016-03-11 16:54:55 +0800

    add suggestion addon

---
+++ Addons/Suggestions/Controller/SuggestionsController.class.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Addons\Suggestions\Controller;
+use Home\Controller\AddonsController;
+
+class SuggestionsController extends AddonsController{
+	function lists(){
+		$model = $this->getModel();
+
+		$list_data = $this->_get_model_list($model);
+		
+		$uids = getSubByKey($list_data['list_data'],'uid');
+		$uids = array_filter($uids);
+		$uids = array_unique($uids);
+		if (!empty($uids)){
+			$map['uid'] = array(
+					'in',
+					$uids
+			);
+			$members = M('member')->where($map)->field('uid,nickname,truename,mobile')->select();
+			foreach ($members as $m) {
+				!empty($m['truename']) || $m['truename'] = $m['nickname'];
+				$user[$m['uid']] = $m;
+			}
+			foreach ($list_data['list_data'] as &$vo) {
+				$vo['mobile'] = $user[$vo['uid']]['mobile'];
+				$vo['uid'] = $user[$vo['uid']]['truename'];
+ 			}
+		}
+		$this->assign($list_data);
+		$this->display($model['template_list']);
+	}
+	
+	function suggest(){
+		$config = getAddonConfig('Suggestions');
+		$this->assign($config);
+
+		$data['uid'] = $this->mid;
+		$user = M('member')->where($data)->find();
+		$this->assign('user',$user);
+
+		if (IS_POST){
+			$truename = I('truename');
+			if ($config['need_truename'] && !empty($truename)){
+				$member['truename'] = $truename;
+			}
+			$mobile = I('mobile');
+			if($config['need_mobile'] && !empty($mobile)){
+				$member['mobile'] = $mobile;
+			}
+			if (!empty($member)){
+				M('member')->where($data)->save($member);
+			}
+			$data['cTime'] = time();
+			$data['content'] = I('content');
+
+			$res = M('suggestions')->add($data);
+			if ($res)
+				$this->success('增加成功！');
+			else
+				$this->error('增加失败！');
+
+		}else{
+			$this->display();
+		}
+	}
+}
