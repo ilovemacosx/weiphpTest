5513a50959a89289de7fd7a380cc4af552e7d13d
ilovemacosx <ilovemacosx@sina.com>
2016-03-11 17:51:08 +0800

    modify suggestion addon 4 times

--- Addons/Suggestions/Model/WeixinAddonModel.class.php
+++ Addons/Suggestions/Model/WeixinAddonModel.class.php
@@ -8,18 +8,16 @@ use Home\Model\WeixinModel;
  */
 class WeixinAddonModel extends WeixinModel{
 	function reply($dataArr, $keywordArr = array()) {
-		/*$this->replyText ( '欢迎来到我的世界-_-' );
 		
 		$param['token'] = get_token();
 		$param['openid'] = get_openid();
 		$url = addons_url('Suggestions://Suggestions/suggest',$param);
-				*/
 		$articles = array(
 						array(
 							'Title' => '建议意见',
 							'Description' => '点击进入填写反馈内容',
 							'PicUrl' => 'http://weiphp.cn/Public/Home/images/about/logo.jpg',
-							'Url' => 'http://www.bing.com'
+							'Url' => $url
 							));
 		$res = $this->replyNews($articles);
 	}
