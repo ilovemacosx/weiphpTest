3d467602f12e6c3f8c194c8d8e19c7d205456b58
ilovemacosx <ilovemacosx@sina.com>
2016-03-12 16:00:02 +0800

    modify suggestions 2 times

--- Addons/ShowSuggestions/Controller/ShowSuggestionsController.class.php
+++ Addons/ShowSuggestions/Controller/ShowSuggestionsController.class.php
@@ -7,6 +7,7 @@ class ShowSuggestionsController extends AddonsController{
 	function showlists(){
 		$data['uid'] = $this->mid;
 		$suggestions = M('suggestions')->where($data)->find();
+		dump($suggestions);
 		$this->assign('suggestions',$suggestions);
 		$this->display();
 	}
